30a2ffd77deb7de38e460fa0013b020f02609590
vnmatt <gordon_matt@hotmail.com>
2018-08-24 20:47:40 +0930

    Refactoring and implemented solution for app restart (for installing/uninstalling plugins)

---
+++ Mantle.Web.ContentManagement/Areas/Admin/ContentBlocks/IContentBlockExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace Mantle.Web.ContentManagement.Areas.Admin.ContentBlocks
+{
+    public static class IContentBlockExtensions
+    {
+        public static string GetTypeFullName(this IContentBlock contentBlock)
+        {
+            var type = contentBlock.GetType();
+            return $"{type.FullName}, {type.Assembly.FullName}";
+        }
+    }
+}
