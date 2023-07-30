09a33e6fa3527ad6ae7141138c94ac0669592408
Matt Gordon <gordon_matt@hotmail.com>
2023-07-30 17:49:06 +0930

    Attempting cleanup of legacy bundling via script and style registrars. W.I.P.

---
+++ Plugins/Mantle.Plugins.Widgets.Bootstrap3/Bootstrap3PluginSettings.cs
@@ -0,0 +1,31 @@
+﻿using Mantle.Web.Configuration;
+
+namespace Mantle.Plugins.Widgets.Bootstrap3;
+
+public class Bootstrap3PluginSettings : BaseResourceSettings
+{
+    #region ISettings Members
+
+    public override string Name => "Plugin: Bootsrap 3 Widgets";
+
+    public override bool IsTenantRestricted => false;
+
+    public override string EditorTemplatePath => "/Plugins/Widgets.Bootstrap3/Views/Shared/EditorTemplates/Settings.cshtml";
+
+    #endregion ISettings Members
+
+    public override ICollection<RequiredResourceCollection> Resources { get; set; }
+
+    public override ICollection<RequiredResourceCollection> DefaultResources => new List<RequiredResourceCollection>
+    {
+        new RequiredResourceCollection
+        {
+            Name = "Bootstrap",
+            Resources = new List<RequiredResource>
+            {
+                new RequiredResource { Type = ResourceType.Script, Order = 0, Path = "https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/js/bootstrap.min.js" },
+                new RequiredResource { Type = ResourceType.Stylesheet, Order = 0, Path = "https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap.min.css" }
+            }
+        }
+    };
+}
