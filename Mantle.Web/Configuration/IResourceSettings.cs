2cfa47c50bb1e94e2508d738c82e04861851d858
Matt <gordon-matt@hotmail.com>
2023-07-30 17:49:06 +0930

    Attempting cleanup of legacy bundling via script and style registrars. W.I.P.

---
+++ Mantle.Web/Configuration/IResourceSettings.cs
@@ -0,0 +1,53 @@
+﻿namespace Mantle.Web.Configuration;
+
+public interface IResourceSettings : ISettings
+{
+    ICollection<RequiredResourceCollection> Resources { get; set; }
+
+    ICollection<RequiredResourceCollection> DefaultResources { get; }
+
+    IEnumerable<RequiredResource> GetResources(ResourceType type, string name);
+}
+
+public abstract class BaseResourceSettings : IResourceSettings
+{
+    public BaseResourceSettings()
+    {
+        Resources = DefaultResources;
+    }
+
+    public abstract string Name { get; }
+
+    public abstract bool IsTenantRestricted { get; }
+
+    public abstract string EditorTemplatePath { get; }
+
+    public abstract ICollection<RequiredResourceCollection> Resources { get; set; }
+
+    public abstract ICollection<RequiredResourceCollection> DefaultResources { get; }
+
+    public IEnumerable<RequiredResource> GetResources(ResourceType type, string name) =>
+        Resources.FirstOrDefault(r => r.Name == name)?.Resources.Where(x => x.Type == type).OrderBy(x => x.Order);
+}
+
+public class RequiredResourceCollection
+{
+    public string Name { get; set; }
+
+    public ICollection<RequiredResource> Resources { get; set; } = new List<RequiredResource>();
+}
+
+public class RequiredResource
+{
+    public ResourceType Type { get; set; }
+
+    public string Path { get; set; }
+
+    public int Order { get; set; }
+}
+
+public enum ResourceType : byte
+{
+    Script = 0,
+    Stylesheet = 1
+}
