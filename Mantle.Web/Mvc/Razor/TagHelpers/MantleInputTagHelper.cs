49b91de4dc5fbae02e4e559141867bc68cbaf517
Matt Gordon <gordon_matt@hotmail.com>
2023-08-03 17:54:25 +0930

    Minor changes

--- Mantle.Web/Mvc/Razor/TagHelpers/MantleInputTagHelper.cs
+++ Mantle.Web/Mvc/Razor/TagHelpers/MantleInputTagHelper.cs
@@ -2,6 +2,7 @@
 using Humanizer;
 using Microsoft.AspNetCore.Mvc.TagHelpers;
 using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.ComponentModel;
 using System.Text.Encodings.Web;
 
 namespace Mantle.Web.Mvc.Razor.TagHelpers;
@@ -42,7 +43,7 @@ public class MantleInputTagHelper : InputTagHelper
 
         string preContent = string.Empty;
         string postContent = string.Empty;
-
+        
         output.TagName = "input";
         if (For.ModelExplorer.ModelType == typeof(bool))
         {
@@ -54,6 +55,22 @@ public class MantleInputTagHelper : InputTagHelper
         {
             output.AddClass("form-control", HtmlEncoder.Default);
             output.Attributes.Add("data-bind", $"value: {Bind ?? For.Name.Camelize()}");
+
+            //string labelText;
+            //if (!string.IsNullOrEmpty(Label))
+            //{
+            //    labelText = Label;
+            //}
+            //else
+            //{
+            //    var modelType = For.ModelExplorer.ModelType;
+            //    var property = modelType.GetProperty(For.Name);
+            //    if (property != null)
+            //    {
+            //        var attribute = property.GetCustomAttribute<LocalizedDisplayName>(); // Can't get.. it's in CMS project..
+            //    }
+            //}
+
             preContent = $@"<div class=""form-group"">{htmlHelper.Label(For.Name, Label, new { @class = "control-label" }).GetString()}";
 
             if (ValidationMessage)
